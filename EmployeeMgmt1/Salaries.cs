acf587bb393c14ae6fbd7bd4b3fd5df2cf01e3e2
MarioEskander <MarioEskander2020@gmail.com>
2023-01-27 21:26:06 +0200

    commit

--- EmployeeMgmt1/Salaries.cs
+++ EmployeeMgmt1/Salaries.cs
@@ -86,6 +86,7 @@ namespace EmployeeMgmt1
 
                 string Query = "Select * from SalaryTb1";
                 SalaryList.DataSource = Con.GetData(Query);
+
             }
             catch (Exception)
             {
