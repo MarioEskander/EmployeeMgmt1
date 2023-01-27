f81f91bf822d72b2fe32dbdda10ac356122668bf
MarioEskander <MarioEskander2020@gmail.com>
2023-01-27 21:15:21 +0200

    added if (EmpNameTb.Text == "")

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -56,7 +56,14 @@ namespace EmployeeMgmt1
             DOBTb.Text = EmployeeList.SelectedRows[0].Cells[4].Value.ToString();
             JDateTb.Text = EmployeeList.SelectedRows[0].Cells[5].Value.ToString();
             DailySalTb.Text = EmployeeList.SelectedRows[0].Cells[6].Value.ToString();
-           
+            if (EmpNameTb.Text == "")
+            {
+                Key = 0;
+            }
+            else
+            {
+                Key = Convert.ToInt32(EmployeeList.SelectedRows[0].Cells[0].Value.ToString());
+            }
         }
 
         private void GetDepartment()
