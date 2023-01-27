48439a6e73d2f772cebbdf6a36441e50c3d110bd
MarioEskander <MarioEskander2020@gmail.com>
2023-01-27 22:20:03 +0200

    added Query = string.Format(Query, Name,Gender,Dep,DOB,JDate,Salary);

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -114,7 +114,7 @@ namespace EmployeeMgmt1
                     string JDate = JDateTb.Value.ToString();
                     int Salary =Convert.ToInt32(DailySalTb.Text);
                     string Query = "insert into EmployeeTb1 values('{0}','{1}',{2},'{3}','{4}',{5})";
-                    
+                    Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary);
                     Con.SetData(Query);
                     ShowEmp();
                     MessageBox.Show("Employee Added!!!");
