2a62c7ed4259524ad3666c944b017bff590579c4
MarioEskander <MarioEskander2020@gmail.com>
2023-01-27 21:57:34 +0200

    commit

--- EmployeeMgmt1/Departments.cs
+++ EmployeeMgmt1/Departments.cs
@@ -86,6 +86,7 @@ namespace EmployeeMgmt1
             }
             else
             {
+
                 Key = Convert.ToInt32(DepList.SelectedRows[0].Cells[0].Value.ToString());
             }
         }
